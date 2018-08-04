405503cc9217e29b8deda65d22106bcff901553d
Oops <792439187@qq.com>
2018-08-04 20:46:08 +0800

    Update socket.tcl

--- socket.tcl
+++ socket.tcl
@@ -2,6 +2,7 @@
 set socketid [socket localhost 6969];
 # 行刷缓存
 # fconfigure $socketid -buffering line;
+# fconfigure $socketid -blocking 0 -buffering line;
 puts -nonewline $socketid "WORLD!";
 flush $socketid;
 gets $socketid msg;
